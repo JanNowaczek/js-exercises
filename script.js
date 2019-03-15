fa3360d49e213637c6728d5bc38f9d392f31a205
Jan Nowaczek <jasiek19_85@o2.pl>
2019-03-15 10:30:25 +0100

    script.js added to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('h1').style.color = 'red';
+
+const zbior=document.querySelectorAll('p');
+let i;
+for (i=0; i < zbior.length; i++) {
+    zbior[i].style.backgroundColor = "yellow"
+}
